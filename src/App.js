3a707f3b7df34552b9a974d3ffbd8126ddb5b510
kushcypher <57324252+kushcypher@users.noreply.github.com>
2020-05-29 15:17:04 +0530

    first inplemententation of Reactjs
    first inplemententation of Reactjs

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBar from "./NavBar";
+import PortfolioSection from "./PortfolioSection";
+import AboutSection from "./AboutSection";
+import ContactSection from "./ContactSection";
+import Footer from './Footer';
+
+const App = () => (
+    <div>
+        <NavBar />
+        <PortfolioSection />
+        <AboutSection />
+        <ContactSection />
+        <Footer />
+    </div>
+);
+
+export default App; 
